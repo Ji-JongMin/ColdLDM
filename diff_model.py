ec3d058bc4a8c5a847a5e4fa862ac53d5801553a
Ji-JongMin <109266108+Ji-JongMin@users.noreply.github.com>
2025-01-21 21:45:34 +0900

    Create diff_model.py

---
+++ diff_model.py
@@ -0,0 +1,304 @@
+from diff_utils import (
+    ModelPrediction,
+    exists,
+    default,
+    cast_tuple,
+    divisible_by,
+    extract,
+    Upsample,
+    Downsample,
+    RMSNorm,
+    SinusoidalPosEmb
+)
+
+class Unet_ddpm(nn.Module):
+    def __init__(
+        self,
+        dim,
+        init_dim=None,
+        out_dim=None,
+        dim_mults=(1, 2, 4, 8),
+        channels=64,
+        self_condition=False,
+        resnet_block_groups=8,
+        learned_variance=False,
+        learned_sinusoidal_cond=False,
+        random_fourier_features=False,
+        learned_sinusoidal_dim=16,
+        sinusoidal_pos_emb_theta=10000,
+        attn_dim_head=32,
+        attn_heads=4,
+        full_attn=None,
+        flash_attn=False
+    ):
+        super().__init__()
+
+        # Initialize dimensions
+        self.channels = channels
+        self.self_condition = self_condition
+        input_channels = channels * (2 if self_condition else 1)
+
+        init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv2d(input_channels * 2, init_dim, 7, padding=3)
+
+        # Define dimensions for each layer
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+
+        # Time embeddings
+        time_dim = dim * 4
+
+        if learned_sinusoidal_cond or random_fourier_features:
+            sinu_pos_emb = RandomOrLearnedSinusoidalPosEmb(learned_sinusoidal_dim, random_fourier_features)
+            fourier_dim = learned_sinusoidal_dim + 1
+        else:
+            sinu_pos_emb = SinusoidalPosEmb(dim, theta=sinusoidal_pos_emb_theta)
+            fourier_dim = dim
+
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(fourier_dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim)
+        )
+
+        # Attention
+        if not full_attn:
+            full_attn = (*((False,) * (len(dim_mults) - 1)), True)
+
+        num_stages = len(dim_mults)
+        full_attn = cast_tuple(full_attn, num_stages)
+        attn_heads = cast_tuple(attn_heads, num_stages)
+        attn_dim_head = cast_tuple(attn_dim_head, num_stages)
+
+        assert len(full_attn) == len(dim_mults)
+
+        FullAttention = partial(Attention, flash=flash_attn)
+
+        # Layers
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, ((dim_in, dim_out), layer_full_attn, layer_attn_heads, layer_attn_dim_head) in enumerate(zip(in_out, full_attn, attn_heads, attn_dim_head)):
+            is_last = ind >= (num_resolutions - 1)
+
+            attn_klass = FullAttention if layer_full_attn else LinearAttention
+
+            self.downs.append(nn.ModuleList([
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                attn_klass(dim_in, dim_head=layer_attn_dim_head, heads=layer_attn_heads),
+                Downsample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out, 3, padding=1)
+            ]))
+
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = FullAttention(mid_dim, heads=attn_heads[-1], dim_head=attn_dim_head[-1])
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        for ind, ((dim_in, dim_out), layer_full_attn, layer_attn_heads, layer_attn_dim_head) in enumerate(zip(*map(reversed, (in_out, full_attn, attn_heads, attn_dim_head)))):
+            is_last = ind == (len(in_out) - 1)
+
+            attn_klass = FullAttention if layer_full_attn else LinearAttention
+
+            self.ups.append(nn.ModuleList([
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                attn_klass(dim_out, dim_head=layer_attn_dim_head, heads=layer_attn_heads),
+                Upsample(dim_out, dim_in) if not is_last else nn.Conv2d(dim_out, dim_in, 3, padding=1)
+            ]))
+
+        default_out_dim = channels * (1 if not learned_variance else 2)
+        self.out_dim = default(out_dim, default_out_dim)
+        self.final_res_block = block_klass(dim * 2, dim, time_emb_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time, x_self_cond=None):
+        if self.self_condition:
+            x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
+            x = torch.cat((x_self_cond, x), dim=1)
+
+        x = self.init_conv(x.float())
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+
+            x = block2(x, t)
+            x = attn(x) + x
+            h.append(x)
+
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x) + x
+        x = self.mid_block2(x, t)
+
+        for block1, block2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+            x = attn(x) + x
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+
+        x = self.final_res_block(x, t)
+        return self.final_conv(x)
+
+
+class Diffusion(nn.Module):
+    def __init__(
+        self,
+        model,
+        encoder,
+        decoder,
+        *,
+        image_size,
+        timesteps=10,
+        sampling_timesteps=None,
+        objective='pred_x0',
+        beta_schedule='linear',
+        schedule_fn_kwargs=dict(),
+        ddim_sampling_eta=0.,
+        auto_normalize=True,
+        offset_noise_strength=0.,
+        min_snr_loss_weight=False,
+        min_snr_gamma=5
+    ):
+        super().__init__()
+        assert not (type(self) == Diffusion and model.channels != model.out_dim)
+        assert not model.random_or_learned_sinusoidal_cond
+
+        self.model = model
+        self.encoder = encoder.eval()
+        self.decoder = decoder.eval()
+        self.channels = self.model.channels
+        self.self_condition = self.model.self_condition
+
+        self.image_size = image_size
+
+        self.objective = objective
+
+        assert objective in {'pred_noise', 'pred_x0', 'pred_v'}, (
+            'objective must be either pred_noise (predict noise) or pred_x0 (predict image start) or pred_v '
+            '(predict v [v-parameterization as defined in appendix D of progressive distillation paper, used in imagen-video successfully])'
+        )
+
+        if beta_schedule == 'linear':
+            beta_schedule_fn = self.linear_beta_schedule
+        else:
+            raise ValueError(f'unknown beta schedule {beta_schedule}')
+
+        betas = beta_schedule_fn(timesteps, **schedule_fn_kwargs)
+
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, dim=0)
+        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.)
+
+        timesteps, = betas.shape
+        self.num_timesteps = int(timesteps)
+
+        # sampling related parameters
+
+        self.sampling_timesteps = default(sampling_timesteps, timesteps)  # default num sampling timesteps to number of timesteps at training
+
+        # helper function to register buffer from float64 to float32
+
+        register_buffer = lambda name, val: self.register_buffer(name, val.to(torch.float32))
+        register_buffer('betas', betas)
+
+        # offset noise strength - in blogpost, they claimed 0.1 was ideal
+
+        self.offset_noise_strength = offset_noise_strength
+        snr = alphas_cumprod / (1 - alphas_cumprod)
+
+        maybe_clipped_snr = snr.clone()
+        if min_snr_loss_weight:
+            maybe_clipped_snr.clamp_(max=min_snr_gamma)
+
+        if objective == 'pred_noise':
+            register_buffer('loss_weight', maybe_clipped_snr / snr)
+        elif objective == 'pred_x0':
+            register_buffer('loss_weight', maybe_clipped_snr)
+        elif objective == 'pred_v':
+            register_buffer('loss_weight', maybe_clipped_snr / (snr + 1))
+
+    @property
+    def device(self):
+        return self.betas.device
+
+    @torch.inference_mode()
+    @torch.cuda.amp.autocast(enabled=False)
+    def q_sample(self, x_start, x_end, t, noise=None):
+        noise = default(noise, lambda: x_end - x_start)
+        return (
+            x_start + extract(self.betas, t, x_start.shape) * x_end
+        )
+
+    def p_losses(self, x_start, cond, t, noise=None, offset_noise_strength=None):
+        b, c, h, w = x_start.shape
+
+        noise = default(noise, lambda: cond - x_start).float()
+
+        offset_noise_strength = default(offset_noise_strength, self.offset_noise_strength)
+
+        if offset_noise_strength > 0.:
+            offset_noise = torch.randn(x_start.shape[:2], device=self.device)
+            noise += offset_noise_strength * rearrange(offset_noise, 'b c -> b c 1 1')
+
+        # noise sample
+
+        x = self.q_sample(x_start=x_start, x_end=noise, t=t, noise=noise)
+        input = torch.cat((x, cond), dim=1)
+
+        x_self_cond = None
+
+        model_out = self.model(input, t, x_self_cond)
+
+        if self.objective == 'pred_noise':
+            target = noise
+        elif self.objective == 'pred_x0':
+            target = x_start
+        elif self.objective == 'pred_v':
+            v = self.predict_v(x_start, t, noise)
+            target = v
+        else:
+            raise ValueError(f'unknown objective {self.objective}')
+
+        loss1 = F.smooth_l1_loss(model_out, target, reduction='none')
+        loss1 = reduce(loss1, 'b ... -> b', 'mean')
+        loss1 = loss1.mean()
+
+        loss2 = 1 - ssim(model_out, target, data_range=1.0)
+
+        loss = loss1
+
+        return loss
+
+    def forward(self, img, cond, *args, **kwargs):
+        b, c, h, w, device, img_size, = *img.shape, img.device, self.image_size
+        assert h == img_size and w == img_size, f'height and width of image must be {img_size}'
+        t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
+
+        img = img.float()
+        cond = cond.float()
+        return self.p_losses(img, cond, t, *args, **kwargs)
+
+    @staticmethod
+    def linear_beta_schedule(timesteps):
+        scale = 10 / timesteps
+        beta_start = scale * 0.1
+        beta_end = scale * 1.0
+        return torch.linspace(beta_start, beta_end, timesteps, dtype=torch.float64)
