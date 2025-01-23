b850610c98cd16b3cdd1986c1f56e00a1109f190
Ji-JongMin <109266108+Ji-JongMin@users.noreply.github.com>
2025-01-23 15:33:06 +0900

    Update diff_utils.py

--- diff_utils.py
+++ diff_utils.py
@@ -64,8 +64,6 @@ class SinusoidalPosEmb(nn.Module):
 
 
 class RandomOrLearnedSinusoidalPosEmb(nn.Module):
-    """ following @crowsonkb 's lead with random (learned optional) sinusoidal pos emb """
-    """ https://github.com/crowsonkb/v-diffusion-jax/blob/master/diffusion/models/danbooru_128.py#L8 """
 
     def __init__(self, dim, is_random = False):
         super().__init__()
